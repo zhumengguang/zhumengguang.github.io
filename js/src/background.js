91f33fefe34a7c4f892bfb2aa8b1182aa62eaf49
zhumengguang <zmengguang@126.com>
2018-04-23 17:38:47 +0800

    Site updated: 2018-04-23 17:38:46

---
+++ js/src/background.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>
+	window.onload = function(){
+		var htmls = '<video autoplay loop poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" style="width: 100%;">'
+				+'<br>'
+				+'<source src="http://thenewcode.com/assets/videos/polina.webm" type="video/webm">'
+				+'<br>'
+				+'<source src="http://thenewcode.com/assets/videos/polina.mp4" type="video/mp4">'
+				+'<br>'
+			    +'</video>';
+	    
+		$('body').append(htmls);
+	}		
+</script>
